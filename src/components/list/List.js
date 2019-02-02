7a75cdf9c63851b5c183439f6110c11920688c68
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-02-02 13:44:52 +0100

    Adding fetch call and helper methof

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { handleResponse } from "../../helper";
 
 class List extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      currencies: [],
+      error: null
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch("https://api.udilia.com/coins/v1/cryptocurrencies?page=1&perPage=20")
+      .then(handleResponse)
+      .then(data => {
+        this.setState({ currencies: data.currencies, loading: false });
+      })
+      .catch(error => {
+        this.setState({ error: error.errorMessage, loading: false });
+      });
+  }
+
   render() {
+    console.log(this.state);
+    if (this.state.loading) {
+      return <div>Loading...</div>;
+    }
     return <div>text</div>;
   }
 }
