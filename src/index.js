96b0c59ad8539aa209c3a0a97f964cf3a1d515e5
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-02-02 18:38:13 +0100

    Adding not found page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/common/Header";
 import List from "./components/list/List";
 import "./index.css";
+import NotFound from "./components/notfound/NotFound";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <List />
-    </div>
+    <BrowserRouter>
+      <Fragment>
+        <Header />
+        <Switch>
+          <Route path="/" component={List} exact />
+          <Route component={NotFound} />
+        </Switch>
+      </Fragment>
+    </BrowserRouter>
   );
 };
 ReactDOM.render(<App />, document.getElementById("root"));
