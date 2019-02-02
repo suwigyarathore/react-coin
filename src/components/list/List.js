d338acec4081ccd20e8d05afa0c780becb32865d
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-02-02 14:32:26 +0100

    Adding crypto currency table and styling

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { handleResponse } from "../../helper";
+import { API_URL } from "../../config";
+import "./Table.css";
+import Loading from "../common/Loading";
 
 class List extends React.Component {
   constructor() {
@@ -13,7 +16,7 @@ class List extends React.Component {
 
   componentDidMount() {
     this.setState({ loading: true });
-    fetch("https://api.udilia.com/coins/v1/cryptocurrencies?page=1&perPage=20")
+    fetch(`${API_URL}/cryptocurrencies?page=1&perPage=20`)
       .then(handleResponse)
       .then(data => {
         this.setState({ currencies: data.currencies, loading: false });
@@ -23,12 +26,60 @@ class List extends React.Component {
       });
   }
 
-  render() {
-    console.log(this.state);
-    if (this.state.loading) {
-      return <div>Loading...</div>;
+  renderChangePercent(percent) {
+    if (percent > 0) {
+      return <span className="percent-raised">{percent}% &uarr;</span>;
+    } else if (percent < 0) {
+      return <span className="percent-fallen">{percent}% &darr;</span>;
+    } else {
+      return <span>{percent}</span>;
     }
-    return <div>text</div>;
+  }
+
+  render() {
+    const { loading, error, currencies } = this.state;
+    if (loading) {
+      return (
+        <div className="loading-container">
+          <Loading />
+        </div>
+      );
+    }
+
+    if (error) {
+      return <div className="error">{error}</div>;
+    }
+    return (
+      <div className="Table-container">
+        <table className="Table">
+          <thead className="Table-head">
+            <tr>
+              <th>Cryptocurreny</th>
+              <th>Price</th>
+              <th>Market Cap</th>
+              <th>24H Change</th>
+            </tr>
+          </thead>
+          <tbody className="Table-body">
+            {currencies.map(currency => (
+              <tr key={currency.name}>
+                <td>
+                  <span className="Table-rank">{currency.rank}</span>
+                  {currency.name}
+                </td>
+                <td>
+                  <span className="Table-dollar">$ {currency.price}</span>
+                </td>
+                <td>
+                  <span className="Table-dollar">$ {currency.marketCap}</span>
+                </td>
+                <td>{this.renderChangePercent(currency.percentChange24h)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
